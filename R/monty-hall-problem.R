6c734b4c67db0dd170b3085cfc7f36d9900c469d
Tyler <118477975+tbnaes@users.noreply.github.com>
2023-09-07 11:09:35 -0400

    Update monty-hall-problem.R

--- R/monty-hall-problem.R
+++ R/monty-hall-problem.R
@@ -35,28 +35,19 @@ create_game <- function()
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   Contestant will select a door for the Monty Hall Problem game.
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `select_door()` randomly chooses a door among the 3 doors.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   The game setup has 3 possibilities, so the contestant must choose
+#'    1 of 3 doors. 
 #'   
 #' @param ... no arguments are used by the function.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a length 1 numerical vector
+#'   indicating the initial pick of the contestant.
 #'   
 #' @examples
 #'   select_door()
@@ -72,28 +63,22 @@ select_door <- function( )
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   One of the two goat doors is opened by the host.
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `open_goat_door()` looks behind the scenes at which doors have goats 
+#'   and opens one goat door that has not been chosen by the contestant.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   The game requires a goat door to be opened after the contestant
+#'    makes the initial selection. This will lead into the contestant 
+#'    choosing to stay or change doors in the following stage of the game. 
 #'   
-#' @param ... no arguments are used by the function.
+#' @param ... game argument is from create_game function and a.pick is from the
+#'    select_door function.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a length 1 numerical vector
+#'   indicating the position of a goat that has not been chosen by the guest.
 #'   
 #' @examples
 #'   open_goat_door(c("goat","goat","car"), 1)
@@ -119,28 +104,30 @@ open_goat_door <- function( game, a.pick )
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   The contestant chooses to stay with initial pick of door or 
+#'    change to the other door not revealed.
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `change_door()` works from a stay argument that indicates
+#'    whether contestant will stick with initial pick or go with
+#'    the other option.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   At this point in the Monty Hall game, the guest has been shown
+#'    one door with a goat behind it. They are either already paired 
+#'    with a winning door or the other door has a car behind it. The guest
+#'    now has the option to stay with their initial pick or change doors.
+#'    This code takes in which door has been opened, and which doors 
+#'    have a goat or a car which is not known to the contestant. The 
+#'    change leads into determining a winner or loser in the final. 
 #'   
-#' @param ... no arguments are used by the function.
+#' @param ... the stay argument takes a logical vector for staying or
+#'    changing doors. The opened.door argument is from the 
+#'    open_goat_door function and the a.pick is from the select_door
+#'    function.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a length 1 numerical vector
+#'   indicating the position of the contestant choice.
 #'   
 #' @examples
 #'   change_door(stay=T,1,1)
@@ -165,28 +152,24 @@ change_door <- function( stay=T, opened.door, a.pick )
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   The contestant is either a winner or a loser!
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `determine_winner()` takes all previous return statements
+#'    and reveals whether the contestant is a winner or a loser.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   At this last stage of the game, the contestant has already
+#'    been given the choice to stay or change doors. Their final
+#'    pick is then measured up against what the code knows to be
+#'    the winner or the loser. If the guest's door is paired with
+#'    car, then they are a winner of the Monty Hall game. 
 #'   
-#' @param ... no arguments are used by the function.
+#' @param ... final.pick is returned from the change_door function. 
+#'    game pulls from the initial arrangement of two goats and a car.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a length 1 character vector
+#'   indicating the final door and game pairing.
 #'   
 #' @examples
 #'   determine_winner(1,c("goat","goat","car"))
@@ -209,28 +192,24 @@ determine_winner <- function( final.pick, game )
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   Pay the Monty Hall game in its entirety.
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `play_game()` uses the entire code already shown to
+#'   output the win or loss based on staying or changing doors.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   The game setup has already been established by the previous
+#'    code, so all that's left is to play the Monty Hall game. By
+#'    using this function, all previous functions are called in order
+#'    such that the final output is a data frame based on the options
+#'    to stay or change doors.  
 #'   
 #' @param ... no arguments are used by the function.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a 2 by 2 data frame
+#'   indicating the Win or Loss matching with Stay or 
+#'   Change doors.
 #'   
 #' @examples
 #'   play_game()
@@ -261,28 +240,23 @@ play_game <- function( )
 
 
 #' @title
-#' #'   Create a new Monty Hall Problem game.
+#' #'   Play the Monty Hall game as many times as you'd like!
 #' 
 #' @description
-#' #'   `create_game()` generates a new game that consists of two doors 
-#'   with goats behind them, and one with a car.
+#' #'   `play_n_games()` is much like the previous function where
+#'    you can run the entire setup, but this function allows for
+#'    an argument of how many games to be played.
 #'   
 #' @details
-#' #'   The game setup replicates the game on the TV show "Let's
-#'   Make a Deal" where there are three doors for a contestant
-#'   to choose from, one of which has a car behind it and two 
-#'   have goats. The contestant selects a door, then the host
-#'   opens a door to reveal a goat, and then the contestant is
-#'   given an opportunity to stay with their original selection
-#'   or switch to the other unopened door. There was a famous 
-#'   debate about whether it was optimal to stay or switch when
-#'   given the option to switch, so this simulation was created
-#'   to test both strategies. 
+#' #'   The Monty Hall game allows for some serious consideration
+#'    of probability statistics. Knowing that some will want to
+#'    run this game several times in order to see the chances
+#'    and study the options, this function has a built in number
+#'    taker that determines how many times the game will be played. 
 #'   
-#' @param ... no arguments are used by the function.
+#' @param ... the n argument takes an integer.
 #' 
-#' @return The function returns a length 3 character vector
-#'   indicating the positions of goats and the car.
+#' @return The function returns a data frame of the outcomes.
 #'   
 #' @examples
 #'   play_n_games(100)
